4682e795b8c4e0e5edbf543bb4c1eaeaaef01919
kosmoknot <kosmoknot@protonmail.com>
2022-11-17 21:15:28 +0530

    added template test

--- test/runtest.sh
+++ test/runtest.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+echo Running tests:
+echo
+echo
+echo 1.Checking template parser output
 
+tempParser=$(diff ./TemplateTest.html ../site/TemplateTest.html -q)
+echo $tempParser
+if [[ -z $tempParser ]]; then
+    echo "Template parser output matched!"
+else
+    echo "ERROR: Template parser output did not match!"
+fi
